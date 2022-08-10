507ccb93f58dee3989187d944d20202c590f791a
Carlos Chin <carlos.chindz@gmail.com>
2022-08-10 13:01:34 -0500

    Project config

---
+++ trigger_script/ScriptableNode.lua
@@ -0,0 +1,2 @@
+--You can use 'params.parameter name' to get the parameters defined in the node. 					
+--For example, if a parameter named 'entity' is defined in the node, you can use 'params.entity' to get the value of the parameter.
