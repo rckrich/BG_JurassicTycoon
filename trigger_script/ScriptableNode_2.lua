62b677dc8a196ce46c5bfe4ce203c30fff99c270
Carlos Chin <carlos.chindz@gmail.com>
2022-08-14 16:34:03 -0500

    Update Items and Buffs
    The following Items were created:
    
    * Dropper
    * ResourceCollector
    * Prop
    * Store
    * Cosntruction
    
    The Following buffs were created
    * Dropper_Generator
    * ResourceCollector_Generator
    * Prop_Generator
    * Store_Generator
    * Construction_Generator

---
+++ trigger_script/ScriptableNode_2.lua
@@ -0,0 +1,11 @@
+--You can use 'params.parameter name' to get the parameters defined in the node. 					
+--For example, if a parameter named 'entity' is defined in the node, you can use 'params.entity' to get the value of the parameter.
+
+function Resource_Collector 
+    while true do
+      wait(0.1)
+      print("This is a test")
+    end
+end
+
+Resource_Collector
